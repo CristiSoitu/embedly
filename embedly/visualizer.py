8d90c97c5c8d38d15c579a15fe27cd82e4d08b86
CristiSoitu <soitu.cristi@gmail.com>
2024-12-20 15:14:15 +0000

    added flexibility in specifying params

--- embedly/visualizer.py
+++ embedly/visualizer.py
@@ -7,22 +7,42 @@ import io
 from dash import Dash, dcc, html, Input, Output, no_update, callback
 import plotly.graph_objects as go
 import pandas as pd
+import webbrowser
 
 
 
-def viz_umap_plot(data_df, embeddings_col_0 = 'umap_embeddings_0', embeddings_col_1 = 'umap_embeddings_1', name_col = 'name', x_px_col = 'x_px', y_px_col = 'y_px', url_col = 'url', cluster_col = 'k_means'):
+
+def viz_umap_plot(df_path = '../data/embeddings.csv', embeddings_col_0 = 'umap_embeddings_0', embeddings_col_1 = 'umap_embeddings_1', url_col = 'url', display_columns = ['name'], width_px = 128, height_px = 128, cluster_col = None, display_fraction=1, open_browser=True):
     '''
     Visualize UMAP embeddings with plotly. Hover over the points to see the thumbnails of the images.
 
-    :param images_df: DataFrame with the images data.
-    :param embeddings_col_0: Column name for the first UMAP embedding.
-    :param embeddings_col_1: Column name for the second UMAP embedding.
-    :param name: Column name for the image name.
-    :param x_px: Column name for the image height.
-    :param y_px: Column name for the image width.
-    :param url: Column name for the image URL.
+    Parameters:
+
+    df_path (str): path to the csv file with the embeddings and other columns
+    embeddings_col_0 (str): name of the column with the first embedding
+    embeddings_col_1 (str): name of the column with the second embedding
+    url_col (str): name of the column with the url of the images
+    display_columns (list): list of columns to display when hovering over the points
+    width_px (int): width of the thumbnail
+    height_px (int): height of the thumbnail
+    cluster_col (str): name of the column with the cluster labels
+    display_fraction (float): fraction of the data to display
+    open_browser (bool): whether to open the browser automatically
+    
     '''
 
+    # Load the data
+    data_df = pd.read_csv(df_path)
+    if display_fraction < 1:
+        data_df = data_df.sample(frac=display_fraction)
+
+    # if there is a cluster column, use it for coloring
+    if cluster_col is not None:
+        data_colors = data_df[cluster_col]
+    else:
+        data_colors = None
+
+    # Create the plot
     fig = go.Figure()
     
     fig.add_trace(
@@ -30,7 +50,7 @@ def viz_umap_plot(data_df, embeddings_col_0 = 'umap_embeddings_0', embeddings_co
             x=data_df[embeddings_col_0],
             y=data_df[embeddings_col_1], 
             mode='markers',
-            marker=dict(size=5, color=data_df[cluster_col], opacity=1),
+            marker=dict(size=5, color=data_colors, opacity=1),
         )
     )
 
@@ -41,7 +61,7 @@ def viz_umap_plot(data_df, embeddings_col_0 = 'umap_embeddings_0', embeddings_co
     )
 
     fig.update_layout(
-        title="UMAP embeddings of MNIST dataset",
+        title="UMAP embeddings of the images",
         xaxis_title="UMAP embeddings 0",
         yaxis_title="UMAP embeddings 1", 
         #center the title
@@ -74,7 +94,6 @@ def viz_umap_plot(data_df, embeddings_col_0 = 'umap_embeddings_0', embeddings_co
         Output("graph-tooltip-2", "bbox"),
         Output("graph-tooltip-2", "children"),
         Output("graph-tooltip-2", "direction"),
-
         Input("graph-2-dcc", "hoverData"),
     )
 
@@ -90,44 +109,50 @@ def viz_umap_plot(data_df, embeddings_col_0 = 'umap_embeddings_0', embeddings_co
         # control the position of the tooltip
         y = hover_data["y"]
         direction = "bottom" if y > 1.5 else "top"
-        x_px = data_df[x_px_col][hover_data["pointNumber"]]
-        y_px = data_df[y_px_col][hover_data["pointNumber"]]
-        name = data_df[name_col][hover_data["pointNumber"]]
-        url = data_df[url_col][hover_data["pointNumber"]]
-        cluster = data_df[cluster_col][hover_data["pointNumber"]]
+
+        #name = data_df.iloc[hover_data["pointNumber"]][name_col]
+        url = data_df.iloc[hover_data["pointNumber"]][url_col]
+
+        display_data = []
+        for col in display_columns:
+            display_data.append(data_df.iloc[hover_data["pointNumber"]][col])
 
         children = [
             html.Img(
                 src=url,
-                style={"width": f"{y_px}px", "height": f"{x_px}px"},
+                style={"width": f"{width_px}px", "height": f"{height_px}px"},
             ),
-            html.P(f'{name}'),
-            html.P(f'Cluster: {cluster}')
-        ]
+
+        ] + [html.P(f'{col}: {display_data[i]}') for i, col in enumerate(display_columns)]
 
         return True, bbox, children, direction
-    #app.run_server(debug=True)
-    app.run(jupyter_mode='external')
 
+    host = '127.0.0.1'
+    port = 8070
+    url = f"http://{host}:{port}/"
+    if open_browser:
+        webbrowser.open(url)
 
+    app.run_server(debug=True, host=host, port=port)
 
 
 if __name__ == '__main__':
 
-    config = toml.load('config.toml')
+    #config = toml.load('config.toml')
 
     parser = argparse.ArgumentParser(description='VizuMap')
-    parser.add_argument('--input_file', type=str, default=config['PARAMS']['INPUT_FILE'],  help='Input file with embeddings')
-    parser.add_argument('--name_col', type=str, default=config['PARAMS']['NAME_COL'], help='Name column')
-    parser.add_argument('--embeddings_col_0', type=str, default=config['PARAMS']['EMBEDDINGS_COL_0'], help='Embeddings column 0')
-    parser.add_argument('--embeddings_col_1', type=str, default=config['PARAMS']['EMBEDDINGS_COL_1'], help='Embeddings column 1')
-    parser.add_argument('--x_px_col', type=str, default=config['PARAMS']['X_PX_COL'], help='X pixel column name')
-    parser.add_argument('--y_px_col', type=str, default=config['PARAMS']['Y_PX_COL'], help='Y pixel column name')
-    parser.add_argument('--url_col', type=str, default=config['PARAMS']['URL_COL'], help='URL column name')
-    parser.add_argument('--cluster_col', type=str, default=config['PARAMS']['CLUSTER_COL'], help='Cluster column name')
+    parser.add_argument('--input_file', type=str, default='../data/embeddings.csv', help='Path to the input file')
+    parser.add_argument('--embeddings_col_0', default='umap_embedding_1', type=str, help='Name of the column with the first embedding')
+    parser.add_argument('--embeddings_col_1', default='umap_embedding_2', type=str, help='Name of the column with the second embedding')
+    parser.add_argument('--url_col', type=str, default='url', help='Name of the column with the url of the images')
+    parser.add_argument('--display_columns', default=['image'], type=str, help='List of columns to display when hovering over the points')
+    parser.add_argument('--width_px', default=128, type=int, help='Width of the thumbnail')
+    parser.add_argument('--height_px', default=128, type=int, help='Height of the thumbnail')
+    parser.add_argument('--cluster_col', default='k_means', type=str, help='Name of the column with the cluster labels')
+    parser.add_argument('--display_fraction', default=1, type=float, help='Fraction of the data to display')
+    parser.add_argument('--open_browser', default=True, type=bool, help='Whether to open the browser automatically')
 
     args = parser.parse_args()
 
-    data_df = pd.read_csv(args.input_file)
-
-    utils.viz_umap_plot(data_df=data_df, embeddings_col_0=args.embeddings_col_0, embeddings_col_1=args.embeddings_col_1, name_col=args.name_col, x_px_col=args.x_px_col, y_px_col=args.y_px_col, url_col=args.url_col, cluster_col=args.cluster_col)
+    viz_umap_plot(df_path=args.input_file, embeddings_col_0=args.embeddings_col_0, embeddings_col_1=args.embeddings_col_1, url_col=args.url_col, display_columns=args.display_columns, width_px=args.width_px, height_px=args.height_px, cluster_col=args.cluster_col, display_fraction=args.display_fraction, open_browser=args.open_browser)
+    
